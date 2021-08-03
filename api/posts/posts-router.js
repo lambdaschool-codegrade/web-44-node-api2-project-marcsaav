7891e78ea63fe863fdf759bc9465d571d4cfc5d2
Marcos Saavedra <marcsaav96@gmail.com>
2021-08-03 11:42:12 -0700

    Server instantiated

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
 // implement your posts router here
+
+const express = require('express')
+
+const postsRouter = express.Router()
+
+const Posts = require('./posts-model')
+
+// [GET] All Posts
+
+// [GET] Post with specified ID
+// [POST] New Post
+// [PUT] Update Post with specified ID
+// [DELETE] Post with specified ID
+// [GET] Comments of Post with specified ID
+
+module.exports = postsRouter
